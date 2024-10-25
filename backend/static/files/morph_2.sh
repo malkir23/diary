366d79ff16f36b7bec2bbc3083f23ddce69786a9
Konstantin <mandragorand@gmail.com>
2024-10-25 13:56:01 +0200

    Refactor download endpoints and add Morph download endpoint

--- backend/static/files/morph_2.sh
+++ backend/static/files/morph_2.sh
@@ -1,78 +1,44 @@
 #!/bin/bash
 
-# Оновлення системи та встановлення залежних компонентів
-echo "Оновлюємо систему та встановлюємо залежності..."
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y make build-essential unzip lz4 gcc git jq ncdu tmux cmake clang pkg-config libssl-dev python3-pip protobuf-compiler bc curl
+# Зупинка раніше створених служб
+echo "Зупинка служб Geth та Morph..."
+sudo systemctl stop gethm
+sudo systemctl stop morphm
 
-# Перевірка наявності Go
-echo "Перевіряємо встановлену версію Go..."
-GO_VERSION=$(go version 2>/dev/null)
-REQUIRED_GO_VERSION="go1.22.2"
-
-if [[ $GO_VERSION == *"$REQUIRED_GO_VERSION"* ]]; then
-  echo "Необхідна версія Go ($REQUIRED_GO_VERSION) вже встановлена."
-else
-  echo "Необхідна версія Go ($REQUIRED_GO_VERSION) не встановлена. Встановлюємо..."
-  sudo rm -rf /usr/local/go
-  curl -Ls https://go.dev/dl/go1.22.2.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-  echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh
-  echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile
-  source /etc/profile.d/golang.sh
-  source ~/.profile
-  go version
-fi
-
-# Створення каталогу для Morph та завантаження репозиторію
-echo "Створюємо каталог для Morph та завантажуємо репозиторій..."
-mkdir -p ~/.morph
-cd ~/.morph
-git clone https://github.com/morph-l2/morph.git
-cd morph
-git checkout v0.2.0-beta
-make nccc_geth
-cd ~/.morph/morph/node
-make build
-
-# Завантаження та розпакування файлів конфігурації Genesis блоку
-echo "Завантажуємо та розпаковуємо конфігураційні файли Genesis блоку..."
-cd ~/.morph
-wget https://raw.githubusercontent.com/morph-l2/config-template/main/holesky/data.zip
-unzip data.zip
-
-# Створення секретного ключа
-echo "Створюємо секретний ключ для зв'язку з вузлом..."
-openssl rand -hex 32 > ~/.morph/jwt-secret.txt
-
-# Завантаження та розпакування знімку (snapshot)
-echo "Завантажуємо та розпаковуємо знімок..."
-wget -q --show-progress https://snapshot.morphl2.io/holesky/snapshot-20240805-1.tar.gz
-tar -xzvf snapshot-20240805-1.tar.gz
-mv snapshot-20240805-1/geth geth-data
-mv snapshot-20240805-1/data node-data
-rm -rf snapshot-20240805-1.tar.gz
-
-# Створення сервісного файлу для Geth
-echo "Створюємо сервісний файл для Geth..."
-sudo tee /etc/systemd/system/gethm.service > /dev/null << EOF
+# Створення нового сервісного файлу для Geth Validator
+echo "Створюємо сервісний файл для Geth Validator..."
+sudo tee /etc/systemd/system/geth_morph.service > /dev/null << EOF
 [Unit]
-Description=Geth
+Description=Geth for Validator
 After=network-online.target
 
 [Service]
 User=root
 ExecStart=/root/.morph/morph/go-ethereum/build/bin/geth \
-     --morph-holesky \
-     --datadir "/root/.morph/geth-data" \
-     --http --http.api=web3,debug,eth,txpool,net,engine \
-     --http.port 8546 \
-     --authrpc.addr localhost \
-     --authrpc.vhosts="localhost" \
-     --authrpc.port 8552 \
-     --authrpc.jwtsecret=/root/.morph/jwt-secret.txt \
-     --miner.gasprice="100000000" \
-     --log.filename=/root/.morph/geth.log \
-     --port 30363
+--datadir=/root/.morph/geth-data \
+--port 30305 \
+--verbosity=3 \
+--http \
+--http.corsdomain="*" \
+--http.vhosts="*" \
+--http.addr=0.0.0.0 \
+--http.port=8547 \
+--http.api=web3,eth,txpool,net,engine \
+--ws \
+--ws.addr=0.0.0.0 \
+--ws.port=8546 \
+--ws.origins="*" \
+--ws.api=web3,eth,txpool,net,engine \
+--networkid=2810 \
+--authrpc.addr="0.0.0.0" \
+--authrpc.port="8552" \
+--authrpc.vhosts="*" \
+--authrpc.jwtsecret=/root/.morph/jwt-secret.txt \
+--gcmode=archive \
+--metrics \
+--metrics.addr=0.0.0.0 \
+--metrics.port=6063 \
+--miner.gasprice="100000000"
 
 Restart=on-failure
 RestartSec=10
@@ -82,27 +48,50 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-# Запуск сервісу Geth
-echo "Запускаємо сервіс Geth..."
+# Запуск нової служби Geth
+echo "Запускаємо нову службу Geth..."
 sudo systemctl daemon-reload
-sudo systemctl enable gethm.service
-sudo systemctl start gethm.service
+sudo systemctl enable geth_morph
+sudo systemctl start geth_morph
 
-# Створення сервісного файлу для Morph Node
-echo "Створюємо сервісний файл для Morph Node..."
-sudo tee /etc/systemd/system/morphm.service > /dev/null << EOF
+# Перевірка та налаштування змінних середовища
+echo "Налаштовуємо змінні середовища..."
+export L1MessageQueueWithGasPriceOracle=0x778d1d9a4d8b6b9ade36d967a9ac19455ec3fd0b
+export START_HEIGHT=1434640
+export Rollup=0xd8c5c541d56f59d65cf775de928ccf4a47d4985c
+export Ethereum_Holesky_beacon_chain_RPC=https://ethereum-holesky-beacon-api.publicnode.com
+
+# Користувач має вказати свої дані
+read -p "Введіть свій Ethereum Holesky RPC (отриманий на Infura): " Ethereum_Holesky_RPC
+read -p "Введіть приватний ключ валідатора: " Your_Validator_Key
+
+export Ethereum_Holesky_RPC=$Ethereum_Holesky_RPC
+export Your_Validator_Key=$Your_Validator_Key
+
+# Створення сервісного файлу для Morph Validator
+echo "Створюємо сервісний файл для Morph Validator..."
+sudo tee /etc/systemd/system/morph_validator.service > /dev/null << EOF
 [Unit]
-Description=Morph Node
+Description=Morph Validator Node
 After=network-online.target
 
 [Service]
 User=root
-ExecStart=/root/.morph/morph/node/build/bin/morphnode \
-     --home /root/.morph/node-data \
-     --l2.jwt-secret /root/.morph/jwt-secret.txt \
-     --l2.eth http://localhost:8546 \
-     --l2.engine http://localhost:8552 \
-     --log.filename /root/.morph/node.log
+ExecStart=/root/.morph/morph/node/build/bin/morphnode --validator --home /root/.morph/node-data \
+ --l2.jwt-secret /root/.morph/jwt-secret.txt \
+ --l2.eth http://localhost:8547 \
+ --l2.engine http://localhost:8552 \
+ --l1.rpc $Ethereum_Holesky_RPC \
+ --l1.beaconrpc $Ethereum_Holesky_beacon_chain_RPC \
+ --l1.chain-id 17000 \
+ --validator.privateKey $Your_Validator_Key \
+ --sync.depositContractAddr $L1MessageQueueWithGasPriceOracle \
+ --sync.startHeight $START_HEIGHT \
+ --derivation.rollupAddress $Rollup \
+ --derivation.startHeight $START_HEIGHT \
+ --derivation.fetchBlockRange 200 \
+ --log.filename /root/.morph/node.log \
+ --metrics-server-enable
 
 Restart=on-failure
 RestartSec=10
@@ -112,10 +101,10 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-# Запуск сервісу Morph Node
-echo "Запускаємо сервіс Morph Node..."
+# Запуск нової служби Morph Validator
+echo "Запускаємо нову службу Morph Validator..."
 sudo systemctl daemon-reload
-sudo systemctl enable morphm.service
-sudo systemctl start morphm.service
+sudo systemctl enable morph_validator
+sudo systemctl start morph_validator
 
-echo "Установка завершена. Нода Morph та Geth запущені."
+echo "Усі служби запущені успішно."
