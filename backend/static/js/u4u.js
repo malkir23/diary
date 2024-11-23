114f8c30a2397a1055e3054aceed407443c2f431
Konstantin <mandragorand@gmail.com>
2024-11-23 15:29:31 +0100

    f

--- backend/static/js/u4u.js
+++ backend/static/js/u4u.js
@@ -3,7 +3,7 @@ const tables = document.querySelectorAll('.table');
 
 tasks.forEach(task => {
     task.addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData('text/plain', task.new-place);
+        event.dataTransfer.setData('text/plain', task.new_place);
     });
 });
 
