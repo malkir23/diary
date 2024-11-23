6e1c16690d81cfb142c4da3e71b8c8db5265a6de
Konstantin <mandragorand@gmail.com>
2024-11-23 18:30:38 +0100

    asc

--- backend/static/js/tasks.js
+++ backend/static/js/tasks.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
 					</td>
 					<td class="task-category">
 							<select value="${task.category_id}" disabled>
-							${categories.map(category => `
+							${Object.values(categories).map((category) => `
 								<option
 									value="${category.id}"
 									${category.id == task.category_id ? "selected" : ""}
