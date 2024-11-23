e97f57605ef856876831bfa79c6d366052b02335
Konstantin <mandragorand@gmail.com>
2024-11-23 19:46:19 +0100

    asfas

--- backend/static/js/tasks.js
+++ backend/static/js/tasks.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 	// Add a task row to the table
 	function addTaskToTable(task) {
-		console.log(new Map(Object.entries(categories)));
+		console.log(new Map(Object.entries(JSON.parse(categories))));
 
 			const row = document.createElement("tr");
 			row.id = `task-row-${task.id}`;
