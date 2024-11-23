5c88915b7a599e5a5115744928d5a3d470b0c6f7
Konstantin <mandragorand@gmail.com>
2024-11-23 16:22:03 +0100

    fix

--- backend/static/js/u4u.js
+++ backend/static/js/u4u.js
@@ -63,10 +63,7 @@ function updateTaskStatus(taskId, newStatus) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-					task_id: taskId,
-					updated_data: { status: newStatus }
-        })
+        body: JSON.stringify({ status: newStatus })
     })
     .then(response => {
         if (!response.ok) {
