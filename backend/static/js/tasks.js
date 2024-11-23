43dd2390690bb3d0960b9d85372d949ac604468b
Konstantin <mandragorand@gmail.com>
2024-11-23 20:58:56 +0100

    asfasf

--- backend/static/js/tasks.js
+++ backend/static/js/tasks.js
@@ -10,7 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 	// Add a task row to the table
 	function addTaskToTable(task) {
-		const validJsonString = categories.replace(/"/g, "'");
+		const validJsonString = categories
+    .replace(/'/g, '"')
+    .replace(/([{,])\s*"?(\w+)"?\s*:/g, '$1"$2":');
 
 			// Parse the JSON string into a JavaScript object
 			const dataCategories = JSON.parse(validJsonString);
